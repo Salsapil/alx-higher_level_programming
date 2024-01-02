bc81ac6d9d38cb6f4cd52f41a00ea519f9ec4e58
Salsabil <salsabeelahmed323@gmail.com>
2024-01-02 18:56:46 +0200

    task11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def pow(a, b):
+    if b == 0:
+        return 1
+    elif b == 1:
+        return a
+    elif b < 0:
+        return 1 / pow(a, -b)
+    else:
+        return a * pow(a, b - 1)
