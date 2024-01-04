08e5834c209784b18eebd45eb43fe5baf64a7a70
Salsabil <salsabeelahmed323@gmail.com>
2024-01-04 23:11:39 +0200

    task100_edit2

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import sys
 
-if __name__ == "__main__":
-    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-    sys.exit(1)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
