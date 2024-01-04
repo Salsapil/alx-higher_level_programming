bc18a6551487c60ba66d80f5cb34e2b1ec68a19b
Salsabil <salsabeelahmed323@gmail.com>
2024-01-04 16:47:22 +0200

    task0_1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-# result_add = add(10, 5)
-# print("10 + 5 = {}".format(result_add))
-# result_sub = sub(10, 5)
-# print("10 - 5 = {}".format(result_sub))
-# result_mul = mul(10, 5)
-# print("10 * 5 = {}".format(result_mul))
-# result_div = div(10, 5)
-# print("10 / 5 = {}".format(result_div))
-
-a = 10
-b = 5
-print(a, "+", b, "=", add(a, b))
-print(a, "-", b, "=", sub(a, b))
-print(a, "*", b, "=", mul(a, b))
-print(a, "/", b, "=", div(a, b))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
