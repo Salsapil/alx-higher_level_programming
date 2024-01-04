160e887df5d0053456c4f607147b73ebda5984ed
Salsabil <salsabeelahmed323@gmail.com>
2024-01-04 17:27:01 +0200

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-    args = sys.argv[1:]
-    len = len(args)
-    if len == 0:
-        print("{} arguments".format(len))
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
     else:
-        print("{} arguments".format(len))
-        print("{}: {}".format(len, args))
+        print("{} arguments:".format(argc))
+
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, sys.argv[i]))
