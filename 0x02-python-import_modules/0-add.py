4b1479844a7edbe228cd7b075bc1d0242960e468
Salsabil <salsabeelahmed323@gmail.com>
2024-01-04 16:41:45 +0200

    task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-
-# result = add(1, 2)
-# print("1 + 2 = {}".format(result))
-
-a = 1
-b = 2
-print(a, "+", b, "=", add(a, b))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print(a, "+", b, "=", add(a, b))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print(int(sys.argv[1]))
+    args = sys.argv[1:]
+    len = len(args)
+    if len == 0:
+        print("{} arguments".format(len))
+    else:
+        print("{} arguments".format(len))
+        print("{}: {}".format(len, args))
