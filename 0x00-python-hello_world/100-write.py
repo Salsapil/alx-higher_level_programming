ed42d74549baff16b9b0e71304cbb18134d2555d
Salsabil <salsabeelahmed323@gmail.com>
2024-01-04 23:09:37 +0200

    task100_edit_0x00

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys
 
-sys.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-sys.exit(1)
+if __name__ == "__main__":
+    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+    sys.exit(1)
