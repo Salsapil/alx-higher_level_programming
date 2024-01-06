1cb2786b9065f44cec3c492ca25bf1feec1e440d
Salsabil <salsabeelahmed323@gmail.com>
2024-01-06 17:21:29 +0200

    task6.1

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j == len(matrix[i]) - 1:
-                print(matrix[i][j], end="")
-            else:
-                print(matrix[i][j], end=" ")
-        print()
+    if matrix:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if j == len(matrix[i]) - 1:
+                    print(matrix[i][j], end="")
+                else:
+                    print(matrix[i][j], end=" ")
+            print()
+    else:
+        return None
