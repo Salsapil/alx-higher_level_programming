01c8328c7fc6b8c976137b272fa870f2bbebfa20
Salsabil <salsabeelahmed323@gmail.com>
2024-01-06 14:15:35 +0200

    task3.5

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    for i in range(len(my_list)):
+        if 
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
 def print_reversed_list_integer(my_list=[]):
-    for i in reversed(range(len(my_list))):
-        print("{:d}".format(my_list[i]))
-
-# def print_reversed_list_integer(my_list=[]):
-#     lenn = len(my_list)
-#     for i in range(lenn):
-#         lenn = lenn - 1
-#         print("{:d}".format(my_list[lenn]))
+    if my_list:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
