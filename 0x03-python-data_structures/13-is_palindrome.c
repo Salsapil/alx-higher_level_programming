dc57f65090443a7b1255078aab2f03d78ca05622
Salsabil <salsabeelahmed323@gmail.com>
2024-01-07 14:31:00 +0200

    task13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * is_palindrome - function
+ * @head: input head of the linked list
+*/
+int is_palindrome(listint_t **head)
+{
+    listint_t *current = *head;
+    int arr[1000000], i = 0, j = 0;
+
+    while (current != NULL)
+    {
+        arr[i] = current->n;
+        current = current->next;
+        i++;
+    }
+    i--;
+    while (j < i)
+    {
+        if (arr[j] != arr[i])
+            return (0);
+        j++;
+        i--;
+    }
+    return (1);
+}
