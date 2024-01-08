170a0032db826d7ed5ade20eec16e9d6c68ec37e
Salsabil <salsabeelahmed323@gmail.com>
2024-01-08 22:38:29 +0200

    task13.1

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,26 +2,39 @@
 /**
  * is_palindrome - function
  * @head: input head of the linked list
+ * return: another function
 */
 int is_palindrome(listint_t **head)
 {
-    listint_t *current = *head;
-    int arr[1000000], i = 0, j = 0;
+    listint_t *left = *head;
+    return (is_palindrome_recursion(&left, *head));
+}
+/**
+ * is_palindrome_helper - function
+ * @left: point to left of list
+ * @right: point to right of list
+ * return: 0 or 1
+*/
+int is_palindrome_recursion(listint_t **left, listint_t *right)
+{
+    int res;
 
-    while (current != NULL)
+    if (right == NULL)
+        return (1);
+
+    res = is_palindrome_recursion(left, right->next);
+    if (res == 0)
     {
-        arr[i] = current->n;
-        current = current->next;
-        i++;
+        return (0);
     }
-    i--;
-    
-    while (j < i)
+    if (right->n == (*left)->n)
     {
-        if (arr[j] != arr[i])
-            return (0);
-        j++;
-        i--;
+        res = 1;
     }
-    return (1);
+    else
+    {
+        res = 0;
+    }
+    *left = (*left)->next;
+    return (res);
 }
