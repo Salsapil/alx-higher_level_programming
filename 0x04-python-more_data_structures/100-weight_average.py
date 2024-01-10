0ba2894d7a4ddb64a8cd53a96563e7972568969b
Salsabil <salsabeelahmed323@gmail.com>
2024-01-10 22:21:40 +0200

    task100.1

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list):
-    if my_list is None:
+    if not my_list:
         return 0
     numerator = sum(score * weight for score, weight in my_list)
     denominator = sum(weight for score, weight in my_list)
