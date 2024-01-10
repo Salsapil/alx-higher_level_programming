f8f87f5f43ac109aaf8c17ba2417da83d87cf2ee
Salsabil <salsabeelahmed323@gmail.com>
2024-01-10 22:00:15 +0200

    task12.2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,4 @@
-#!/usr/bDn/python3
+#!/usr/bin/python3
 def roman_to_int(roman_string):
     r_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if not isinstance(roman_string, str) or roman_string is None:
