347f68df6587d07056a79618e180221f0bc54829
Salsabil <salsabeelahmed323@gmail.com>
2024-01-10 21:02:34 +0200

    task10.2

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     max_value = 0
+    # will be key with max value. If negative, function will return None.
     max_key = None
     for key, value in a_dictionary.items():
         if value > max_value:
