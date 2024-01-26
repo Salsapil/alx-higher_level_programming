58798f86ca5e50048d11b5366e8897bd0102042b
Salsabil <salsabeelahmed323@gmail.com>
2024-01-26 11:25:05 +0200

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """define square"""
+
+    def __init__(self, size):
+        """Method in a class
+
+        Args:
+            size: The first parameter. length"""
+        self.__size = size
