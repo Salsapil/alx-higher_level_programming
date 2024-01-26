54d6909e1020d6b550738061075a3e4cc03eec9b
Salsabil <salsabeelahmed323@gmail.com>
2024-01-26 11:42:55 +0200

    task3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """define square"""
+
+    def __init__(self, size=0):
+        """Method in a class
+
+        Args:
+            size: The first parameter. length"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Public instance method
+            return: current square area"""
+        return self.__size ** 2
