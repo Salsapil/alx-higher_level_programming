94b03ec4a6f819e0cc07f69fca24f84f78471d90
Salsabil <salsabeelahmed323@gmail.com>
2024-01-29 17:03:28 +0200

    Classes_Task9.1

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -93,4 +93,7 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """Class Method that use cls"""
-        return cls(size, size)
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            return cls(size, size)
