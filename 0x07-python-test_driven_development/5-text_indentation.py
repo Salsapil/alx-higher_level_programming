ff81d824da2eb8f917700c49b9e228bc99a73853
Salsabil <salsabeelahmed323@gmail.com>
2024-01-31 11:50:47 +0200

    TDD_5.1

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
+"""
+module:
+This is the fourth task
+in Test-Driven-Development
+"""
+
+
 def text_indentation(text):
+    """text identation
+
+    Args:
+        text: First arg
+
+    Raises:
+        TypeError: must be a string
+    """
     if type(text) != str:
         raise TypeError("text must be a string")
     for char in ".?:":
         text = text.replace(char, char + "\n\n")
     print("\n".join(line.strip() for line in text.split("\n")))
+
 
 if __name__ == "__main__":
     import doctest
