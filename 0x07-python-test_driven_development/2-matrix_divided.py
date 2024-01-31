0742f81fd3b80a6ede8386b76606d882a013b709
Salsabil <salsabeelahmed323@gmail.com>
2024-02-01 00:26:47 +0200

    last_TDD

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 """
 module:
-This is the second task 
+This is the second task
 in Test-Driven-Development
 """
 
+
 def matrix_divided(matrix, div):
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(element / div, 2) for element in row] for row in matrix]
+
 
 if __name__ == "__main__":
     import doctest
