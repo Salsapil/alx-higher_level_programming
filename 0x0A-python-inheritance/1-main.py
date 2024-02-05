b961125265357fcecd604039d510d8ee2ecde62c
Salsabil <salsabeelahmed323@gmail.com>
2024-02-05 19:45:26 +0200

    test4

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -2,8 +2,7 @@
 MyList = __import__('1-my_list').MyList
 
 my_list = MyList()
-my_list.append(Hi)
-my_list.append(4)
+my_list.print_sorted([1, 5, 9], 1)
 my_list.append(2)
 my_list.append(3)
 my_list.append(5)
