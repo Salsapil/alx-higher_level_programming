b16a332feb858c2b4b93312c92743806686a7983
Salsabil <salsabeelahmed323@gmail.com>
2024-02-06 01:35:40 +0200

    inheritance6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module for class"""
+
+
+class BaseGeometry:
+    """class"""
+    def area(self):
+        """calcuate area"""
+        raise Exception("area() is not implemented")
