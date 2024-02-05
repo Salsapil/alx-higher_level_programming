586565cabcf72e46d181145e263c73586b756003
Salsabil <salsabeelahmed323@gmail.com>
2024-02-06 00:10:04 +0200

    test6.3

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 MyList = __import__('1-my_list').MyList
 
-my_list = MyList()
+my_list = MyList(MyList([1, 5, -4, 8, 2, 'Hi']))
 print(my_list)
 my_list.print_sorted()
-print(MyList([1, 5, -4, 8, 2], 1))
+print(my_list)
