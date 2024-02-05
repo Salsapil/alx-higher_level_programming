2151ed64a52f93498ad8f5db9c13c1ebd344359f
Salsabil <salsabeelahmed323@gmail.com>
2024-02-06 00:21:11 +0200

    inheritance2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+"""module"""
+
+
 def is_same_class(obj, a_class):
+    """is same class:
+    checks if the object is exactly an instance of the specified class"""
+    return type(obj) is a_class
