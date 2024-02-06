4ce687a01d3c5241e34f39a2a1e9d84ef1b91307
Salsabil <salsabeelahmed323@gmail.com>
2024-02-07 01:19:44 +0200

    InputOutput10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        try:
+            for i in attrs:
+                if not isinstance(attrs, str):
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        dict_new = dict()
+        for i, j in self.__dict__.items():
+            if i in attrs:
+                dict_new[i] = j
+        return dict_new
