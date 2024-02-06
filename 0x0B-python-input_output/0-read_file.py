8ada1d2b6e1a378889f72ba5973725459065ecef
Salsabil <salsabeelahmed323@gmail.com>
2024-02-06 23:40:06 +0200

    InputOutput0.1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""module"""
+
+
 def read_file(filename=""):
-    with open(filename, encoding='utf-8') as file:
+    """read file"""
+    with open(filename, encoding='UTF8') as file:
         print(file.read(), end="")
