f3dbc6e185c66352c6cf5ba4967c6eaaf8aca305
Salsabil <salsabeelahmed323@gmail.com>
2024-02-11 21:12:35 +0200

    Almost_a_Circle100

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import json
+import csv
 
 
 class Base:
@@ -47,5 +48,40 @@ class Base:
             if data:
                 list_dicts = cls.from_json_string(data)
                 return [cls.create(**d) for d in list_dicts]
+            else:
+                return "[]"
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline="") as file:
+            writer = csv.writer(file)
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    writer.writerow([obj.id, obj.width,
+                                    obj.height, obj.x, obj.y])
+                elif cls.__name__ == "Square":
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        filename = cls.__name__ + ".csv"
+        with open(filename, "r") as file:
+            reader = csv.reader(file)
+            if reader:
+                instances = []
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        instance = cls.create(
+                            id=int(row[0]), width=int(row[1]),
+                            height=int(row[2]), x=int(row[3]), y=int(row[4])
+                            )
+                    elif cls.__name__ == "Square":
+                        instance = cls.create(
+                            id=int(row[0]), size=int(row[1]),
+                            x=int(row[2]), y=int(row[3])
+                            )
+                    instances.append(instance)
+                return instances
             else:
                 return "[]"
