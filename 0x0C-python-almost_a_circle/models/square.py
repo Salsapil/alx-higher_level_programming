f57197f79edcf9df108d3ee58d645b3734b5e936
Salsabil <salsabeelahmed323@gmail.com>
2024-02-12 23:30:49 +0200

    test9

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,25 +9,37 @@ class Square(Rectangle):
     """sub class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """__init__"""
+        """Initializes"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """__str__"""
+        """str"""
         return f"[Square] ({self.id}) {self.x}/{self.y} \
 - {self.width}"
 
     @property
     def size(self):
-        """property"""
+        """
+        getter for size property
+        
+        Returns:
+            int: The size of the rectangle.
+        """
         return self.width
 
     @size.setter
     def size(self, value):
+        """
+        setter for size property
+        
+        Args:
+            value (int): The new size of the rectangle.
+        """
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """update dict"""
         if args:
             if len(args) >= 1:
                 self.id = args[0]
@@ -42,4 +54,5 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """save to dict"""
         return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
