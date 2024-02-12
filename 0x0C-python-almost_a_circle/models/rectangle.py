4882c30d2bb0caeabebcedf05db197ef859e3075
Salsabil <salsabeelahmed323@gmail.com>
2024-02-12 23:09:41 +0200

    test7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""module"""
+
+"""
+This module defines the `Rectangle` class
+"""
+
 from models.base import Base
 
 
@@ -7,7 +11,7 @@ class Rectangle(Base):
     """sub class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """init"""
+        """Initializes a new Rectangle object."""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -16,10 +20,26 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        Getter for the width property.
+
+        Returns:
+            int: The width of the rectangle.
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        Setter for the width property.
+
+        Args:
+            value (int): The new width of the rectangle.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -28,10 +48,26 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """
+        Getter for the height property.
+
+        Returns:
+            int: The height of the rectangle.
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        Setter for the height property.
+
+        Args:
+            value (int): The new height of the rectangle.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -40,10 +76,26 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """
+        Getter for the x-coordinate property.
+
+        Returns:
+            int: The x-coordinate of the rectangle.
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """
+        Setter for the x property.
+
+        Args:
+            value (int): The new x of the rectangle.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
@@ -52,10 +104,26 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """
+        Getter for the y property.
+
+        Returns:
+            int: The y of the rectangle.
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """
+        Setter for the y-coordinate property.
+
+        Args:
+            value (int): The new y-coordinate of the rectangle.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
@@ -63,17 +131,21 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """area of rectangle"""
         return self.width * self.height
 
     def display(self):
+        """display special character"""
         print("\n" * self.y, end="")
         print(((" " * self.x) + "#" * self.width + "\n") * self.height, end="")
 
     def __str__(self):
+        """"str"""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} \
 - {self.width}/{self.height}"
 
     def update(self, *args, **kwargs):
+        """update dic"""
         if args:
             if len(args) >= 1:
                 self.id = args[0]
@@ -90,6 +162,7 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """save to dic"""
         return {
             'x': self.x,
             'y': self.y,
