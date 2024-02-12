0109b56c624adf2f9ef0f9178ddc9af3b685736b
Salsabil <salsabeelahmed323@gmail.com>
2024-02-12 18:12:29 +0200

    test2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,8 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """sub class"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """__init__"""
         super().__init__(id)
