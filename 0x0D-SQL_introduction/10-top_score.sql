a407a72f7a43401ce6f5c0972f5505004017f1bd
Salsabil <salsabeelahmed323@gmail.com>
2024-03-12 20:05:52 +0200

    mysql_task10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- display specific columns in a table sort by score.
+SELECT name, score FROM second_table ORDER BY score DESC;
