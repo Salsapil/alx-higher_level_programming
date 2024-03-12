0df0d6b2bd37593455633957621f696a0d5ab933
Salsabil <salsabeelahmed323@gmail.com>
2024-03-12 15:40:51 +0200

    mysql_task1.1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
--- a script that lists all databases of your MySQL server.
--- show all databases
+-- a script that creates the database hbtn_0c_0 in your MySQL server.
+-- create databases
 CREATE DATABASE hbtn_0c_0;
