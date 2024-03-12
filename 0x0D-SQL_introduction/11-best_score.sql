d42ebbbe788fe223726a4b709ee8469d31867c04
Salsabil <salsabeelahmed323@gmail.com>
2024-03-12 20:12:07 +0200

    mysql_task11.1

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- a script that lists all records with a score of the database hbtn_0c_0 in your MySQL server.
 -- Select the best
-SELECT score, name FROM second_table ORDER BY score DESC WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
