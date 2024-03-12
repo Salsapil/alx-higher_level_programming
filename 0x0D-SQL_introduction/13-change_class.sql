d58331956d5a99169d4589888e77ab97262c637d
Salsabil <salsabeelahmed323@gmail.com>
2024-03-12 20:21:23 +0200

    mysql_task13.1

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,3 @@
 -- a script that removes all score records of the database hbtn_0c_0 in your MySQL server.
 -- delete rows
-delete from second_table WHERE score <= 5;
+DELETE FROM second_table WHERE score <= 5;
