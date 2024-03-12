0e148ca171d04e1e14ef68445e4d5c05797a2d8a
Salsabil <salsabeelahmed323@gmail.com>
2024-03-12 20:50:58 +0200

    mysql_task15.1

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- a script that lists the number of records with the same score of the database hbtn_0c_0 in your MySQL server.
 -- number by score
-SELECT score, Count(*) as number FROM second_table GROUP BY score;
+SELECT score, Count(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
