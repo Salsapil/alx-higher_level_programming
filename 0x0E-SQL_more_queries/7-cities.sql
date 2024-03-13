3eb72d7fb888487d06b9c485481882d87059416c
Salsabil <salsabeelahmed323@gmail.com>
2024-03-13 16:51:38 +0200

    mysql_more_task7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- a script that creates the database hbtn_0d_usa and the table cities.
+-- creates the database and the table.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT PRIMARY KEY NOT NULL,
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
+);
