6e740432d711bee2777a30d6b01e38eee1df47eb
Salsabil <salsabeelahmed323@gmail.com>
2024-04-08 14:09:20 +0200

    7. I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = parseInt(process.argv[2]);
+if (isNaN(args)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let x = 0; x < args; x++) {
+    console.log('C is fun');
+  }
+}
