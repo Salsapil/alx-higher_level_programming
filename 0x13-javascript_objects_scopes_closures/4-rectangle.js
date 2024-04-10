229fbe3add9da2589ff19fae443d67539b44721b
Salsabil <salsabeelahmed323@gmail.com>
2024-04-10 03:35:21 +0200

    Task3&4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return 'Regtangle' + {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    if (this.width !== undefined && this.height !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+}
+module.exports = Rectangle;
