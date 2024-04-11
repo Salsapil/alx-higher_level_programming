4122fe4ffaeee190d0382e31a85ccd0479c36588
Salsabil <salsabeelahmed323@gmail.com>
2024-04-11 17:33:48 +0200

    Task6 edit

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
-class Square extends require('./4-rectangle.js') {
-  constructor (size) {
-    super(size, size);
-  }
-
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
-}
-module.exports = Square;
+};
