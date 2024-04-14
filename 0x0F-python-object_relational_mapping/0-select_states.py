87f921591beb0d6691491ee73919b5f4b040f0c6
Salsabil <salsabeelahmed323@gmail.com>
2024-04-14 17:06:57 +0200

    edit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM `states` ORDER BY `id`")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,9 +4,10 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
+
     conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM `states` ORDER BY `id`")
     query_rows = cur.fetchall()
     for row in query_rows:
         if (row[1][0] == "N"): {
