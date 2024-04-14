267b5dffc28a9dba7fc2ea451708042b1e7693a2
Salsabil <salsabeelahmed323@gmail.com>
2024-04-15 00:46:02 +0200

    task6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/home/salsabil/alx-higher_level_programming/.venv/bin/python
+"""state Module"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """states Class"""
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
