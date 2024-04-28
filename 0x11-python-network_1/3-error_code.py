b5b49a01b19aba46903fc98dacb60c63f587f899
Salsabil <salsabeelahmed323@gmail.com>
2024-04-28 19:54:41 +0300

    tasks3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""displays the body of the response"""
+import sys
+from urllib import request
+from urllib import error
+
+
+def main():
+    url = sys.argv[1]
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+
+    except error.HTTPError as error:
+        print("Error code: {}".format(error.code))
+
+
+if __name__ == "__main__":
+    main()
