a786ecc6b6637cd6f20bff6a0c653faf72155cce
Salsabil <salsabeelahmed323@gmail.com>
2024-04-28 19:04:49 +0300

    edit3

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,8 +9,8 @@ def main():
     with request.urlopen(url) as response:
         body = response.read()
     print("Body response:")
-    print("\t- Type: {}".format(type(body)))
-    print("\t- Content: {}".format(body))
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
     print("\t- utf8 content: {}".format(body.decode('utf-8')))
 
 
